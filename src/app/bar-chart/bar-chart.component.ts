f68f5981d26e21aaeecd6d4c6cb8d7f027c4c72b
David Parish <dparish@gmail.com>
2016-10-11 15:55:46 -0500

    showing axis

--- src/app/bar-chart/bar-chart.component.ts
+++ src/app/bar-chart/bar-chart.component.ts
@@ -1,7 +1,10 @@
+/**
+ * Chart example sandbox
+ * from: https://bost.ocks.org/mike/bar/3
+ */
 import { Component, OnInit, ElementRef } from '@angular/core';
 import { ViewChild } from '@angular/core/src/metadata/di';
 import * as d3 from 'd3';
-import { element } from 'protractor';
 
 @Component({
   selector: 'app-bar-chart',
@@ -24,37 +27,51 @@ export class BarChartComponent implements OnInit {
       {name: "Kwon",     value: 42}
     ];
 
-    let width = 960;
-    let height = 500;
+    let margin = {top: 20, right: 30, bottom: 30, left: 40};
+    let width = 960 - margin.left - margin.right;
+    let height = 500 - margin.top - margin.bottom;
+
+    let x = d3.scale.ordinal()
+      .rangeRoundBands([0, width], .1);
+
     let y = d3.scale.linear()
       .range([height, 0]);
 
-    let chart = d3.select(this.element.nativeElement)
-      .attr('height', height)
-      .attr('width', width);
+    let xAxis = d3.svg.axis()
+      .scale(x)
+      .orient('bottom');
 
+    let yAxis = d3.svg.axis()
+      .scale(y)
+      .orient('left');
+
+    let chart = d3.select(this.element.nativeElement)
+      .attr('height', height + margin.top + margin.bottom)
+      .attr('width', width + margin.left + margin.right)
+      .append("g")
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+    x.domain(data.map((d: KeyValue) => d.name));
     y.domain([0, d3.max(data, (d: KeyValue) => d.value)]);
 
-    let barWidth = width / data.length;
+    chart.append('g')
+      .attr('class', 'x axis')
+      .attr('transform', 'translate(0,' + height + ')')
+      .call(xAxis);
 
-    let bar = chart.selectAll('g')
+    chart.append('g')
+      .attr('class', 'y axis')
+      .call(yAxis);
+
+    chart.selectAll('.bar')
       .data(data)
-      .enter().append('g')
-      .attr('transform', (d: KeyValue, i: number): string => 'translate(' + i * barWidth + ',0)');
-
-    bar.append('rect')
+      .enter().append('rect')
+      .attr('class', 'bar')
       .attr('y', (d: KeyValue) => y(d.value))
+      .attr('x', (d: KeyValue) => x(d.name))
       .attr('height', (d: KeyValue) => height - y(d.value))
-      .attr('width', barWidth - 1);
-
-    bar.append('text')
-      .attr('x', barWidth / 2)
-      .attr('y', (d: KeyValue) => y(d.value) + 3)
-      .attr('dy', '.75em')
-      .text((d: KeyValue) => d.value);
+      .attr('width', x.rangeBand());
   }
-
-
 }
 
 interface KeyValue {

cc34d3d6be504684b510a5850a7e736a72cd845f
David Parish <dparish@gmail.com>
2016-10-11 15:16:51 -0500

    converted to svg

--- src/app/bar-chart/bar-chart.component.ts
+++ src/app/bar-chart/bar-chart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 import { ViewChild } from '@angular/core/src/metadata/di';
 import * as d3 from 'd3';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-bar-chart',
@@ -9,25 +10,51 @@ import * as d3 from 'd3';
 })
 export class BarChartComponent implements OnInit {
 
-  @ViewChild('chart') element: ElementRef;
+  @ViewChild('chartSvg') element: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
-    let data = [4, 8, 15, 16, 23, 42];
+    let data: KeyValue[] = [
+      {name: "Locke",    value:  4},
+      {name: "Reyes",    value:  8},
+      {name: "Ford",     value: 15},
+      {name: "Jarrah",   value: 16},
+      {name: "Shephard", value: 23},
+      {name: "Kwon",     value: 42}
+    ];
 
+    let width = 420;
+    let barHeight = 20;
     let x = d3.scale.linear()
-      .domain([0, d3.max(data)])
-      .range([0, 420]);
+      .range([0, width]);
 
-    d3.select(this.element.nativeElement)
-      .selectAll('div')
+    let chart = d3.select(this.element.nativeElement)
+      .attr('height', barHeight * data.length)
+      .attr('width', width);
+
+    x.domain([0, d3.max(data, (d: KeyValue) => d.value)]);
+
+    let bar = chart.selectAll('g')
       .data(data)
-      .enter()
-      .append('div')
-      .style('width', (d: number): string => { return x(d) + 'px'; })
-      .classed('bar', true)
-      .text(function(d: number):string { return d + ''; });
+      .enter().append('g')
+      .attr('transform', (d: KeyValue, i: number): string => 'translate(0,' + i * barHeight + ')');
+
+    bar.append('rect')
+      .attr('width', (d: KeyValue) => x(d.value))
+      .attr('height', barHeight - 1);
+
+    bar.append('text')
+      .attr('x', (d: KeyValue) => x(d.value) - 3)
+      .attr('y', barHeight / 2)
+      .attr('dy', '.35em')
+      .text((d: KeyValue) => d.value);
   }
 
+
+}
+
+interface KeyValue {
+  name: string;
+  value: number;
 }
